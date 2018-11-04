f801f3071038b60cbf782d6628f545c174de2478
Marissa Andersen <marissaandersen@marissas-air.dhcp.nd.edu>
2018-11-04 11:36:47 -0500

    scatter plot made for question 2

--- Exercise9.py
+++ Exercise9.py
@@ -3,6 +3,7 @@ Created on Fri Nov  2 10:37:18 2018
 
 @author: marissaandersen
 """
+#Question 1
 import pandas as pd
 data=pd.read_csv("GPA.txt", header=0, sep=",")
 
@@ -13,7 +14,17 @@ a+xlab("Grade Point Average (GPA)")+ylab("Attendence (days)")+stat_smooth(method
 
 
 
+#Question 2 
+import pandas as pd 
+datafile=pd.read_csv("data.txt", header=0, sep=",")
 
+from plotnine import *
+#bar graph of means of the four populations 
+#make new DF with 5 columns (one for each )
+
+#scatter plot
+a=ggplot(datafile,aes(x="region", y="observations"))+geom_jitter(size=0.01)+theme_classic()
+a+xlab("Region")+ylab("observation")
 
 
 
