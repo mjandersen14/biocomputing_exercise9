92329d66d08f56f28118a37700724866043118a4
Marissa Andersen <marissaandersen@Marissas-MacBook-Air.local>
2018-11-06 14:51:00 -0500

    not many changes made

--- Exercise9.py
+++ Exercise9.py
@@ -20,11 +20,12 @@ datafile=pd.read_csv("data.txt", header=0, sep=",")
 
 from plotnine import *
 #bar graph of means of the four populations 
-#make new DF with 5 columns (one for each )
+#make new DF with 5 columns (one for each region)
+
 
 #scatter plot
 a=ggplot(datafile,aes(x="region", y="observations"))+geom_jitter(size=0.01)+theme_classic()
-a+xlab("Region")+ylab("observation")
+a+xlab("Region")+ylab("Observation")
 
 
 
