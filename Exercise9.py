4a67e551c01b2a00ed14d2d25f9ed4dc95bc5098
Marissa Andersen <marissaandersen@Marissas-MacBook-Air.local>
2018-11-08 22:19:18 -0500

    almost done. did the bar graph a very round about way, will try to fix

--- Exercise9.py
+++ Exercise9.py
@@ -13,53 +13,50 @@ a+xlab("Grade Point Average (GPA)")+ylab("Attendence (days)")+stat_smooth(method
 
 
 
-
 #Question 2 
 import pandas as pd 
 datafile=pd.read_csv("data.txt", header=0, sep=",")
-
 from plotnine import *
-#bar graph of means of the four populations 
-#make new DF with 5 columns (one for each region)
+#Bargraph
+n=pd.DataFrame(columns=['North'])
+s=pd.DataFrame(columns=['South'])
+e=pd.DataFrame(columns=['East'])
+w=pd.DataFrame(columns=['West'])
+for i in range(0,len(datafile)):
+    if  datafile.iloc[i,0]=='north':
+        N=datafile.iloc[i,1]
+        n=n.append({'North':N},ignore_index=True)
+    elif datafile.iloc[i,0]=='south':
+        S=datafile.iloc[i,1]
+        s=s.append({'South':S},ignore_index=True)
+    elif datafile.iloc[i,0]=='east':
+        E=datafile.iloc[i,1]
+        e=e.append({'East':E},ignore_index=True)
+    else:
+        W=datafile.iloc[i,1]
+        w=w.append({'West':W},ignore_index=True)
+all=pd.concat([n,s,e,w],axis=1,sort=True) 
+meannorth=sum(all.North)/len(all)
+meansouth=sum(all.South)/len(all)
+meaneast=sum(all.East)/len(all)  
+meanwest=sum(all.West)/len(all)
+
+means=pd.DataFrame(columns=["Region","Mean"])
+means=means.append({"Region":'North',"Mean":meannorth},ignore_index=True)
+means=means.append({"Region":'South',"Mean":meansouth},ignore_index=True)
+means=means.append({"Region":'East',"Mean":meaneast},ignore_index=True)
+means=means.append({"Region":'West',"Mean":meanwest},ignore_index=True)
+print (means)
+d=ggplot(means)+theme_classic()+xlab("Region")+ylab("Mean")
+d+geom_bar(aes(x="Region",y="Mean"), stat="identity")
 
 
-#scatter plot
+
+
+
+#Scatter plot
 a=ggplot(datafile,aes(x="region", y="observations"))+geom_jitter(size=0.01)+theme_classic()
 a+xlab("Region")+ylab("Observation")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
