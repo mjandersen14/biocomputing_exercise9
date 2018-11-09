f459003c9a91acb4eef8f9cf24cc68d8aabca516
Marissa Andersen <marissaandersen@Marissas-MacBook-Air.local>
2018-11-08 22:38:44 -0500

    finished

--- Exercise9.py
+++ Exercise9.py
@@ -6,18 +6,43 @@ Created on Fri Nov  2 10:37:18 2018
 #Question 1
 import pandas as pd
 data=pd.read_csv("GPA.txt", header=0, sep=",")
-
 from plotnine import *
 a=ggplot(data,aes(x="GPA", y="Attendence"))+geom_point()+theme_classic()
 a+xlab("Grade Point Average (GPA)")+ylab("Attendence (days)")+stat_smooth(method="lm")
-
 
 
 #Question 2 
 import pandas as pd 
 datafile=pd.read_csv("data.txt", header=0, sep=",")
 from plotnine import *
-#Bargraph
+import numpy as np
+#Bar graph
+d=ggplot(datafile)+theme_classic()+xlab("Region")+ylab("Mean of Observations")
+d+geom_bar(aes(x="region",y="observations"),stat="summary",fun_y=np.mean)
+#Scatter plot
+a=ggplot(datafile,aes(x="region", y="observations"))+geom_jitter(size=0.01)+theme_classic()
+a+xlab("Region")+ylab("Observation")
+#these two graphs tell us two different stories. the bar graph just gives us a broad 
+#overview of what is going on,it almost makes us assume all the regions are the 
+#same  we don't see that in the south there are two distinct groupings of observations, how 
+#tight the north observations are, how uniform the west observations are etc.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#so i spent a really long time on this and don't want to throw it away so im leaving it here.. please ignore
+#Bargraph(the really long way..)
 n=pd.DataFrame(columns=['North'])
 s=pd.DataFrame(columns=['South'])
 e=pd.DataFrame(columns=['East'])
@@ -40,36 +65,14 @@ meannorth=sum(all.North)/len(all)
 meansouth=sum(all.South)/len(all)
 meaneast=sum(all.East)/len(all)  
 meanwest=sum(all.West)/len(all)
-
 means=pd.DataFrame(columns=["Region","Mean"])
 means=means.append({"Region":'North',"Mean":meannorth},ignore_index=True)
 means=means.append({"Region":'South',"Mean":meansouth},ignore_index=True)
 means=means.append({"Region":'East',"Mean":meaneast},ignore_index=True)
 means=means.append({"Region":'West',"Mean":meanwest},ignore_index=True)
 print (means)
-d=ggplot(means)+theme_classic()+xlab("Region")+ylab("Mean")
+d=ggplot(means)+theme_classic()+xlab("Region")+ylab("Mean of Observations")
 d+geom_bar(aes(x="Region",y="Mean"), stat="identity")
-
-
-
-
-
-#Scatter plot
-a=ggplot(datafile,aes(x="region", y="observations"))+geom_jitter(size=0.01)+theme_classic()
-a+xlab("Region")+ylab("Observation")
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
